20879ad168ad30333b9cf8515a69a06454992aa4
amregalado <regalado.adriana@gmail.com>
2022-08-26 15:39:00 -0400

    Created base model to test routes with

---
+++ models/walls.js
@@ -0,0 +1,36 @@
+const walls = [
+  {
+    // img: "/images/mcgee.jpg",
+    yearPhotoWasTaken: 2010,
+    location: "New York, New York",
+    artist: "Barry McGee",
+  },
+  // {
+  //     img: url(images/guache.jpg),
+  //     yearPhotoWasTaken: 2019,
+  //     location: "Bogota, Colombia",
+  //     artist: "Guache"
+  // },
+  // {
+  //     img: url(images/delta.jpg),
+  //     yearPhotoWasTaken: 1985,
+  //     location: "Amsterdam, Netherlands",
+  //     artist: "Delta (Boris Tellegen)"
+  // },
+  // {
+  //     img: url(images/keith.jpg),
+  //     yearPhotoWasTaken: 1980,
+  //     location: "New York, New York",
+  //     artist: "Keith Haring"
+  // }
+];
+
+module.exports = walls;
+
+// WALLS SCHEMA
+// const walls {
+//     img: ???,
+//     year: Number,
+//     location: string,
+//     artist: string
+// }
--- server.js
+++ server.js
@@ -1,12 +1,54 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000
+const port = 3000;
+const walls = require("./models/walls.js");
 
+//MIDDLEWARE
+app.use(express.static("public"));
+app.use("/images", express.static(__dirname + "images"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+// app.get("/", (req, res) => {
+//   res.send("Walls, Scrawls and Stalls in Progress");
+// });
 
+// app.get("/walls", (req, res) => {
+//   res.send(walls);
+// });
 
+//INDEX ROUTE
+app.get("/walls", (req, res) => {
+  res.render("index.ejs", {
+    walls: walls,
+  });
+});
 
+// // NEW ROUTE
+// app.get("/walls/new", (req, res) => {
+//     res.render("new.ejs");
+//   });
 
-app.listen(port, ()=> {
-    console.log(`Listening on port ${port}`);
-})
+//   // SHOW ROUTE
+//   app.get("/walls/:id", (req, res) => {
+//     res.render("show.ejs", {
+//       walls: walls[req.params.id],
+//     });
+//   });
+
+//   // NEW CREATE ROUTE
+//   app.post("/walls", (req, res) => {
+//     // console.log(req.body);
+//     walls.push(req.body);
+//     res.redirect("/walls");
+//   });
+
+//   // DELETE ROUTE
+//   app.delete("/walls/:id", (req, res) => {
+//     walls.splice(req.params.id, 1);
+//     res.redirect("/walls");
+//   });
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
