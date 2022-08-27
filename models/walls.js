9772dcc25f87e1831acd29b6d9b55fdfc939ab79
amregalado <regalado.adriana@gmail.com>
2022-08-27 00:11:38 -0400

    Home and index routes created

--- models/walls.js
+++ models/walls.js
@@ -1,36 +1,14 @@
-const walls = [
-  {
-    // img: "/images/mcgee.jpg",
-    yearPhotoWasTaken: 2010,
-    location: "New York, New York",
-    artist: "Barry McGee",
-  },
-  // {
-  //     img: url(images/guache.jpg),
-  //     yearPhotoWasTaken: 2019,
-  //     location: "Bogota, Colombia",
-  //     artist: "Guache"
-  // },
-  // {
-  //     img: url(images/delta.jpg),
-  //     yearPhotoWasTaken: 1985,
-  //     location: "Amsterdam, Netherlands",
-  //     artist: "Delta (Boris Tellegen)"
-  // },
-  // {
-  //     img: url(images/keith.jpg),
-  //     yearPhotoWasTaken: 1980,
-  //     location: "New York, New York",
-  //     artist: "Keith Haring"
-  // }
-];
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-module.exports = walls;
+// WALL SCHEMA
+const wallSchema = new Schema({
+  year: Number,
+  location: String,
+  artist: String,
+}, {timestamps: true});
 
-// WALLS SCHEMA
-// const walls {
-//     img: ???,
-//     year: Number,
-//     location: string,
-//     artist: string
-// }
+// MODEL
+const Wall = mongoose.model("Wall", wallSchema);
+
+module.exports = Wall;
--- server.js
+++ server.js
@@ -1,14 +1,74 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const walls = require("./models/walls.js");
+const methodOverride = require("method-override");
+const { render } = require("ejs");
+
+// MODELS
+const Wall = require("./models/walls.js");
+
+// SETUP MONGOOSE//DEPENDENCIES
+const mongoose = require("mongoose");
+// Importing Model
+// const Wall = require("./models/walls.js");
+// console.log(Wall);
+//Config
+const mongoURI = "mongodb://localhost:27017/walls";
+const db = mongoose.connection;
+
+// Connect to Mongo
+mongoose.connect(mongoURI, () => {
+  console.log("The connection with mongod is established");
+});
 
 //MIDDLEWARE
 app.use(express.static("public"));
-app.use("/images", express.static(__dirname + "images"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(methodOverride("_method"));
 
+const manyWalls = [
+  {
+    year: 2010,
+    location: "New York, New York",
+    artist: "Barry McGee",
+  },
+  {
+    year: 2019,
+    location: "Bogota, Colombia",
+    artist: "Guache",
+  },
+  {
+    year: 1985,
+    location: "Amsterdam, Netherlands",
+    artist: "Delta (Boris Tellegen)",
+  },
+  {
+    year: 1980,
+    location: "New York, New York",
+    artist: "Keith Haring",
+  },
+];
+
+// Wall.create(testWall, (err, wall) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(wall);
+//   }
+//   db.close();
+// });
+
+Wall.insertMany(manyWalls, (error, walls) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(walls);
+  }
+  // db.close();
+});
+
+//DEFAULT
 // app.get("/", (req, res) => {
 //   res.send("Walls, Scrawls and Stalls in Progress");
 // });
@@ -17,37 +77,39 @@ app.use(express.json());
 //   res.send(walls);
 // });
 
+// HOME ROUTE
+app.get("/wss-home", (req, res) => {
+  res.render("home.ejs");
+});
+
 //INDEX ROUTE
 app.get("/walls", (req, res) => {
-  res.render("index.ejs", {
-    walls: walls,
+  Wall.find({}, (error, walls) => {
+    res.render("index.ejs", { walls });
   });
 });
 
-// // NEW ROUTE
-// app.get("/walls/new", (req, res) => {
-//     res.render("new.ejs");
-//   });
+// SHOW
 
-//   // SHOW ROUTE
-//   app.get("/walls/:id", (req, res) => {
-//     res.render("show.ejs", {
-//       walls: walls[req.params.id],
-//     });
-//   });
+// CREATE
+app.post("/walls", (req, res) => {
+  Wall.create(req.body, (error, createdWall) => {
+    if (error) {
+      console.log("error", error);
+      res.send(error);
+    } else {
+      res.send(createdWall);
+    }
+  });
+  res.redirect("/wall");
+});
 
-//   // NEW CREATE ROUTE
-//   app.post("/walls", (req, res) => {
-//     // console.log(req.body);
-//     walls.push(req.body);
-//     res.redirect("/walls");
-//   });
+// HOME ROUTE
+// app.get("/walls", (req
 
-//   // DELETE ROUTE
-//   app.delete("/walls/:id", (req, res) => {
-//     walls.splice(req.params.id, 1);
-//     res.redirect("/walls");
-//   });
+// DESTROY
+
+// EDIT
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
