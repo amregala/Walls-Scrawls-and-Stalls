c52c029e7232f52cae0e68955acc5dc6c65dc225
amregalado <regalado.adriana@gmail.com>
2022-08-28 02:43:13 -0400

    Sign in route functional

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,10 +28,28 @@ router.post("/register", (req, res) => {
   });
 });
 
-router.get('/signin', (req, res) => {
-  res.render('users/signin.ejs')
-  })
+router.get("/signin", (req, res) => {
+  res.render("users/signin.ejs");
+});
 
+router.post("/signin", (req, res) => {
+  User.findOne({ username: req.body.username }, (err, foundUser) => {
+    if (foundUser) {
+      const validLogin = bcrypt.compareSync(
+        req.body.password,
+        foundUser.password
+      );
+      if (validLogin) {
+        req.session.currentUser = foundUser;
+        res.send("User logged in");
+      } else {
+        res.send("Invalid username or password");
+      }
+    } else {
+      res.send("Invalid username or password");
+    }
+  });
+});
 
 module.exports = router;
 
