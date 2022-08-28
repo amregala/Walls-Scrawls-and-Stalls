972e87ea7985bc287916b8d8a39aba048b19f63a
amregalado <regalado.adriana@gmail.com>
2022-08-28 01:59:52 -0400

    Updated userController, register post route created with password hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,30 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+
+//USER MODEL
+const User = require("../models/users");
+
 const router = express.Router();
 
-router.get('/register', (req, res) => {
-  res.render('users/register.ejs');
+router.get("/register", (req, res) => {
+  res.render("users/register.ejs");
+});
+
+router.post("/register", (req, res) => {
+  const salt = bcrypt.genSaltSync(10);
+  // console.log(req.body);
+  req.body.password = bcrypt.hashSync(req.body.password, salt);
+  // console.log(req.body);
+  User.findOne({ username: req.body.username }, (err, userExists) => {
+    if (userExists) {
+      res.send("That username is taken");
+    } else {
+      User.create(req.body, (err, createdUser) => {
+        console.log(createdUser);
+        res.send("User created");
+      });
+    }
+  });
 });
 
 module.exports = router;
