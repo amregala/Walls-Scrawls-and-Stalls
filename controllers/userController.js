8d212413a5c4c3df6e0e3bb51209b8e4c0f0b466
amregalado <regalado.adriana@gmail.com>
2022-08-28 01:26:14 -0400

    Register page created and working

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send("user controller work");
+router.get('/register', (req, res) => {
+  res.render('users/register.ejs');
 });
 
 module.exports = router;
